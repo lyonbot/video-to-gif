d09a07e5302d359e90245074daf434ad71450471
lyonbot <lyonbot@outlook.com>
2024-04-14 03:32:46 +0800

    fix: vite worker

--- src/ffmpeg.ts
+++ src/ffmpeg.ts
@@ -1,7 +1,7 @@
 import { FFmpeg } from "@ffmpeg/ffmpeg";
 import { fetchFile, toBlobURL } from '@ffmpeg/util';
 import { updateStore } from "./store";
-import workerURL from '@ffmpeg/ffmpeg/worker?url';
+// import workerURL from '@ffmpeg/ffmpeg/worker?worker';
 
 export async function initFFmpeg() {
   let ffmpeg = new FFmpeg()
@@ -9,16 +9,16 @@ export async function initFFmpeg() {
   const baseURL = 'https://unpkg.com/@ffmpeg/core@' + CORE_VERSION + '/dist/esm'
 
   // Load the WASM file and coreURL
-  const [coreURL, wasmURL, classWorkerURL] = await Promise.all([
+  const [coreURL, wasmURL] = await Promise.all([
     toBlobURL(`${baseURL}/ffmpeg-core.js`, 'text/javascript'),
     toBlobURL(`${baseURL}/ffmpeg-core.wasm`, 'application/wasm'),
-    workerURL
   ])
+  const classWorkerURL = new URL('@ffmpeg/ffmpeg/worker', import.meta.url)
 
   await ffmpeg.load({
     coreURL,
     wasmURL,
-    classWorkerURL,
+    classWorkerURL: classWorkerURL.toString(),
   });
 
   updateStore({ ffmpeg })
