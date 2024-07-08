045e5dcdf4af9a87dee8772e0bdda3ce011d49c4
WofWca <wofwca@protonmail.com>
2024-07-08 18:46:43 +0400

    refactor: bundle @ffmpeg/core instead of CDN
    - improves security in case the CDN goes malicious,
    - reliability against the CDN going down or being unavailable
    - allows to use the app with no / limited internet access,
        such as the context of browser extensions
    
    `toBlobURL` is no longer required. As the docs say,
    they're there to hack around CORS issues.
    https://ffmpegwasm.netlify.app/docs/getting-started/usage/

--- src/ffmpeg.ts
+++ src/ffmpeg.ts
@@ -1,22 +1,13 @@
 import { FFmpeg } from "@ffmpeg/ffmpeg";
-import { fetchFile, toBlobURL } from '@ffmpeg/util';
 import { updateStore } from "./store";
 import workerURL from '@ffmpeg/ffmpeg/worker?url';
 
 export async function initFFmpeg() {
   let ffmpeg = new FFmpeg()
-  const CORE_VERSION = '0.12.6'
-  const baseURL = 'https://unpkg.com/@ffmpeg/core@' + CORE_VERSION + '/dist/esm'
-
-  // Load the WASM file and coreURL
-  const [coreURL, wasmURL] = await Promise.all([
-    toBlobURL(`${baseURL}/ffmpeg-core.js`, 'text/javascript'),
-    toBlobURL(`${baseURL}/ffmpeg-core.wasm`, 'application/wasm'),
-  ])
 
   await ffmpeg.load({
-    coreURL,
-    wasmURL,
+    coreURL: './ffmpeg-core.js',
+    wasmURL: './ffmpeg-core.wasm',
     classWorkerURL: import.meta.env.PROD ? undefined : workerURL, // vite bug
   });
 
