447f64f26ca222e499297b2d5f054ac82e2c79bf
lyonbot <lyonbot@outlook.com>
2025-03-26 14:51:23 +0800

    feat: auto try usable ffmpeg-core cdn address

--- src/ffmpeg.ts
+++ src/ffmpeg.ts
@@ -1,18 +1,46 @@
 import { FFmpeg } from "@ffmpeg/ffmpeg";
-import { fetchFile, toBlobURL } from '@ffmpeg/util';
+import { toBlobURL } from '@ffmpeg/util';
 import { updateStore } from "./store";
 import workerURL from '@ffmpeg/ffmpeg/worker?url';
 
+declare const FFMPEG_CORE_VERSION: string; // injected by vite
+let baseUrls = [
+  `https://unpkg.com/@ffmpeg/core@${FFMPEG_CORE_VERSION}/dist/esm`,
+  `https://cdn.jsdelivr.net/npm/@ffmpeg/core@${FFMPEG_CORE_VERSION}/dist/esm`,
+  `${import.meta.env.BASE_URL}ffmpeg-core/esm`,
+]
+
 export async function initFFmpeg() {
   let ffmpeg = new FFmpeg()
-  const CORE_VERSION = '0.12.6'
-  const baseURL = 'https://unpkg.com/@ffmpeg/core@' + CORE_VERSION + '/dist/esm'
 
-  // Load the WASM file and coreURL
-  const [coreURL, wasmURL] = await Promise.all([
-    toBlobURL(`${baseURL}/ffmpeg-core.js`, 'text/javascript'),
-    toBlobURL(`${baseURL}/ffmpeg-core.wasm`, 'application/wasm'),
-  ])
+  const preferredBaseURLKey = 'video-to-gif/ffmpeg-core-url'
+  const preferredBaseURL = localStorage.getItem(preferredBaseURLKey)
+  if (preferredBaseURL) {
+    baseUrls = [preferredBaseURL, ...baseUrls.filter(i => i !== preferredBaseURL)]
+  }
+
+  let coreURL = '';
+  let baseURL = '';
+  for (const iBaseURL of baseUrls) {
+    try {
+      const abortController = new AbortController();
+      const timer = setTimeout(() => abortController.abort('timeout'), 10000);
+      const response = await fetch(`${iBaseURL}/ffmpeg-core.js`, { signal: abortController.signal });
+      clearTimeout(timer);
+      if (!response.ok) continue;
+
+      coreURL = URL.createObjectURL(await response.blob());
+      baseURL = iBaseURL;
+      localStorage.setItem(preferredBaseURLKey, baseURL);
+      break;
+    } catch (e) {
+      console.error('Failed to use FFmpeg core from ' + iBaseURL, e);
+      continue;
+    }
+  }
+
+  if (!coreURL) throw new Error('No valid FFmpeg core URL found');
+  const wasmURL = await toBlobURL(`${baseURL}/ffmpeg-core.wasm`, 'application/wasm');
 
   await ffmpeg.load({
     coreURL,
--- vite.config.ts
+++ vite.config.ts
@@ -4,21 +4,28 @@ import solidPlugin from 'vite-plugin-solid';
 import devtools from 'solid-devtools/vite';
 import UnoCSS from 'unocss/vite';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { readFileSync } from 'node:fs';
 
 let GIT_REVISION = 'unknown'
 try { GIT_REVISION = childProcess.execSync("git rev-parse HEAD").toString().trim() } catch { }
 
+const ffmpegCoreVersion = JSON.parse(readFileSync('node_modules/@ffmpeg/core/package.json', 'utf-8')).version;
+
 export default defineConfig({
   plugins: [
     UnoCSS(),
-    /* 
+    /*
     Uncomment the following line to enable solid-devtools.
     For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
     */
     devtools(),
     solidPlugin(),
     viteStaticCopy({
-      targets: [{ src: './LICENSE', dest: './' }]
+      targets: [
+        { src: './LICENSE', dest: './' },
+        { src: './README.md', dest: './' },
+        { src: './node_modules/@ffmpeg/core/dist/esm', dest: './ffmpeg-core' },
+      ]
     })
   ],
   server: {
@@ -29,6 +36,7 @@ export default defineConfig({
     target: 'esnext',
   },
   define: {
+    FFMPEG_CORE_VERSION: JSON.stringify(ffmpegCoreVersion),
     GIT_REVISION: JSON.stringify(GIT_REVISION)
   }
 });
